2a9b8ebc9f95a33bcd3a3386f9e13b1cf2787b57
minty@appl3s <johnathanmin8@gmail.com>
2024-08-24 18:59:03 +1000

    sprite classes

---
+++ src/main/java/tamagotchi/Sprite.java
@@ -0,0 +1,14 @@
+package syncs_hack2024;
+
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Sprite {
+    public float[2] position = {0, 0};
+    public Image image;
+
+    public void draw() {
+        // Draw the sprite at the position
+    }
+}
