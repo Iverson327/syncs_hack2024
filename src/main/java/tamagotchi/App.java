25d709e4eeec616964a86ee06f362b92f9fb1e43
Iverson327 <iverson20040327@gmail.com>
2024-08-24 19:08:33 +1000

    t

--- src/main/java/tamagotchi/App.java
+++ src/main/java/tamagotchi/App.java
@@ -15,16 +15,6 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // GameEngine model = GameEngine.getEasyEngine();
-        GameWindow window = new GameWindow(model);
-        window.run();
-
-        primaryStage.setTitle("Tomogotchi");
-        primaryStage.setScene(window.getScene());
-        primaryStage.setWidth(414);
-        primaryStage.setHeight(896);
-        primaryStage.show();
-
-        // window.run();
+        
     }
 }
