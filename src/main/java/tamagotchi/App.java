d553f28f7a7bf28cc2c39d4a00c1d8e2947c6325
Iverson327 <iverson20040327@gmail.com>
2024-08-24 17:46:13 +1000

    App.java added buttons

--- src/main/java/tamagotchi/App.java
+++ src/main/java/tamagotchi/App.java
@@ -2,6 +2,12 @@ package tamagotchi;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene; 
+import javafx.scene.control.Button; 
+import javafx.scene.layout.*; 
+import javafx.event.ActionEvent; 
+import javafx.event.EventHandler; 
+import javafx.scene.control.Label; 
 // import tamagotchi.engine.GameEngine;
 // import tamagotchi.engine.GameWindow;
 
@@ -9,22 +15,51 @@ import java.util.Map;
 
 public class App extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    public void start(Stage s) 
+    { 
+        // set title for the stage 
+        s.setTitle("creating buttons"); 
+  
+        // create a button 
+        Button b = new Button("button"); 
+  
+        // create a stack pane 
+        Pane r = new Pane(); 
+  
+        // create a label 
+        Label l = new Label("button not selected"); 
+  
+        // action event 
+        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() { 
+            public void handle(ActionEvent e) 
+            { 
+                l.setText("   button   selected    "); 
+            } 
+        }; 
+  
+        // when button is pressed 
+        b.setOnAction(event); 
+  
+        // add button 
+        b.setLayoutX(10); // Sets the X co-ordinate
+        b.setLayoutY(10);
+        l.setLayoutX(10); // Sets the X co-ordinate
+        l.setLayoutY(50);
+        r.getChildren().add(b); 
+        r.getChildren().add(l); 
 
-    @Override
-    public void start(Stage primaryStage) {
-        // GameEngine model = GameEngine.getEasyEngine();
-        // GameWindow window = new GameWindow(model);
-        // window.run();
-
-        // primaryStage.setTitle("Space Invaders");
-        // primaryStage.setScene(window.getScene());
-        // primaryStage.setWidth(window.getWidth());
-        // primaryStage.setHeight(window.getHeight());
-        // primaryStage.show();
-
-        // window.run();
+        // create a scene 
+        Scene sc = new Scene(r, 200, 200); 
+  
+        // set the scene 
+        s.setScene(sc); 
+  
+        s.show(); 
+    } 
+  
+    public static void main(String args[]) 
+    { 
+        // launch the application 
+        launch(args); 
     }
 }
