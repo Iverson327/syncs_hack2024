615d1ee6b54008c558c7427ccb0fd7bd9703ee1a
minty@appl3s <johnathanmin8@gmail.com>
2024-08-24 20:26:56 +1000

    Afadf

--- src/main/java/tamagotchi/GameWindow.java
+++ src/main/java/tamagotchi/GameWindow.java
@@ -26,6 +26,7 @@ class GameWindow {
     public Pane            gamePane;
     public Scene           gameScene;
     public GraphicsContext gamegc;
+    Image gameBackgroundImage = new Image("res/summer_haze.png", true);
 
     // home: 0,
     // game: 1,
@@ -50,6 +51,7 @@ class GameWindow {
         gamegc = gamecanvas.getGraphicsContext2D();
         
         activeStage.setScene(homeScene);
+        activeStage.setScene(gameScene);
     }
 
     void run() {
@@ -71,7 +73,51 @@ class GameWindow {
             homegc.clearRect(0, 0, width, height);
         }
         case 1: {
-            
+            int boxHeight = 500;
+            // Clear
+            gamegc.clearRect(0, 0, width, height);
+
+            // Draw pixel art
+            gamegc.drawImage(-width / 15, -height / 15, 16 * 13, 9 * 13);
+
+            // Draw background of base
+            gamegc.setFill(Paint.valueOf("#db6c39"));
+            gamegc.setFont(new Font(20));
+            gamegc.fillRect(0, boxHeight, width, height - boxHeight);
+
+            // Draw text
+            int happinessHeight = boxHeight + height / 10;
+            int healthHeight = boxHeight + 2 * height / 10;
+            gamegc.fillText("Happiness", width / 20, happinessHeight);
+            gamegc.fillText("Health", width / 20, healthHeight);
+
+            // Draw happiness bar
+            gamegc.setFill(Paint.valueOf("#b85223"));
+            gamegc.fillRect(width - 300,
+                            happinessHeight,
+                            40 * 5 + 4 * 5 + 4,
+                            40 + 4 * 2);
+            gamegc.setFill(Paint.valueOf("#fac011"));
+            for (int i = 0, i < 5; ++i) {
+                gamegc.fillRect(300 + 4,
+                                happinessHeight + 4,
+                                40,
+                                40);
+            }
+
+            // Draw health bar
+            gamegc.setFill(Paint.valueOf("#b85223"));
+            gamegc.fillRect(width - 300,
+                            healthHeight,
+                            40 * 5 + 4 * 5 + 4,
+                            40 + 4 * 2);
+            gamegc.setFill(Paint.valueOf("#e3380e"));
+            for (int i = 0, i < 5; ++i) {
+                gamegc.fillRect(300 + 4,
+                                healthHeight + 4,
+                                40,
+                                40);
+            }
         }
         }
 
