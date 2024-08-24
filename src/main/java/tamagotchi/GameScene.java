548fa89d12aa33aacfa5e7c24b6c2872682cb236
minty@appl3s <johnathanmin8@gmail.com>
2024-08-24 21:51:10 +1000

    imported

--- src/main/java/tamagotchi/GameScene.java
+++ src/main/java/tamagotchi/GameScene.java
@@ -1,5 +1,24 @@
 package tamagotchi;
 
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.Font;
+import javafx.util.Duration;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Paint;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
+import java.io.File;
+
 class GameScene {
     public GameWindow ctx;
     
