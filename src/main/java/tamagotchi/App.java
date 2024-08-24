8c41f96fc34c954c3dc4cbe832506a38e55e0476
minty@appl3s <johnathanmin8@gmail.com>
2024-08-24 19:56:10 +1000

    sadfa

--- src/main/java/tamagotchi/App.java
+++ src/main/java/tamagotchi/App.java
@@ -10,14 +10,11 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        GameWindow window = new GameWindow(new BallPit(640, 400, 1.0/60));
+        GameWindow window = new GameWindow(primaryStage);
         window.run();
 
-        Scene scene;
-        Pane pane;
-
         primaryStage.setTitle("Tomogotchi");
-        primaryStage.setScene(window.getScene());
+        // primaryStage.setScene(window.getScene());
         primaryStage.show();
 
         window.run();
--- src/main/java/tamagotchi/GameWindow.java
+++ src/main/java/tamagotchi/GameWindow.java
@@ -11,22 +11,45 @@ import javafx.scene.text.Font;
 import javafx.util.Duration;
 
 class GameWindow {
-    private final GraphicsContext gc;
-    private Scene scene;
-    private BallPit model;
+    public final GraphicsContext gc;
+    public width = 414;
+    public height = 896;
 
-    GameWindow(BallPit model) {
-        this.model = model;
+    // Home stuff (main menu)
+    public Canvas          homeCanvas;
+    public Pane            homePane;
+    public Scene           homeScene;
+    public GraphicsContext homegc;
 
-        Pane pane = new Pane();
-        this.scene = new Scene(pane, model.getWidth(), model.getHeight());
-        Canvas canvas = new Canvas(model.getWidth(), model.getHeight());
-        gc = canvas.getGraphicsContext2D();
-        pane.getChildren().add(canvas);
-    }
+    // Game stuff
+    public Canvas          gameCanvas;
+    public Pane            gamePane;
+    public Scene           gameScene;
+    public GraphicsContext gamegc;
 
-    Scene getScene() {
-        return this.scene;
+    // home: 0,
+    // game: 1,
+    // etc : 2
+    int activeScene = 0;
+
+    public Stage activeStage;
+
+    GameWindow(Stage primaryStage) {
+        activeStage = primaryStage;
+
+        homeCanvas = new Canvas(width, height);
+        homePane = new Pane();
+        homeScene = new Scene(homePane, width, height);
+        homePane.getChildren().add(homeCanvas);
+        homegc = homecanvas.getGraphicsContext2D();
+
+        gameCanvas = new Canvas(width, height);
+        gamePane = new Pane();
+        gameScene = new Scene(gamePane, width, height);
+        gamePane.getChildren().add(gameCanvas);
+        gamegc = gamecanvas.getGraphicsContext2D();
+        
+        activeStage.setScene(homeScene);
     }
 
     void run() {
@@ -37,24 +60,39 @@ class GameWindow {
         timeline.play();
     }
 
+    public void switchScene(Scene scene, int number) {
+        activeScene = number;
+        activeStage.setScene(scene);
+    }
+
     private void draw() {
-        model.tick();
-
-        gc.clearRect(0, 0, model.getWidth(), model.getHeight());
-
-        gc.setFill(Paint.valueOf("BLACK"));
-        gc.setTextAlign(TextAlignment.LEFT);
-        gc.setFont(new Font(10));
-        gc.fillText(model.getBalls().get(0).observer.update(), 0, 10);
-        gc.fillText(model.getBalls().get(1).observer.update(), 0, 90);
-        gc.fillText(model.getBalls().get(2).observer.update(), 0, 170);
-
-        for (Ball ball: model.getBalls()) {
-            gc.setFill(ball.getColour());
-            gc.fillOval(ball.getxPos() - ball.getRadius(),
-                        ball.getyPos() - ball.getRadius(),
-                        ball.getRadius() * 2,
-                        ball.getRadius() * 2);
+        switch (activeScene) {
+        case 0: {
+            homegc.clearRect(0, 0, width, height);
         }
+        case 1: {
+            
+        }
+        }
+
+
+        // model.tick();
+
+        // gc.clearRect(0, 0, model.getWidth(), model.getHeight());
+
+        // gc.setFill(Paint.valueOf("BLACK"));
+        // gc.setTextAlign(TextAlignment.LEFT);
+        // gc.setFont(new Font(10));
+        // gc.fillText(model.getBalls().get(0).observer.update(), 0, 10);
+        // gc.fillText(model.getBalls().get(1).observer.update(), 0, 90);
+        // gc.fillText(model.getBalls().get(2).observer.update(), 0, 170);
+
+        // for (Ball ball: model.getBalls()) {
+        //     gc.setFill(ball.getColour());
+        //     gc.fillOval(ball.getxPos() - ball.getRadius(),
+        //                 ball.getyPos() - ball.getRadius(),
+        //                 ball.getRadius() * 2,
+        //                 ball.getRadius() * 2);
+        // }
     }
 }
